895bda6cdbb8d4891d12eeca3797a05a56b1390e
Paulo Bruno <paulo.bf111@gmail.com>
2022-12-20 16:59:39 -0300

    Until application.properties

---
+++ src/main/java/com/example/spring_boot_from_scratch/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.spring_boot_from_scratch.student;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService student) {
+        this.studentService = student;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+}
